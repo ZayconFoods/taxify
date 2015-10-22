8921c1080e5237cf4099b0c51f067c84ba061c4b
Tony DeStefano <tony@zayconfoods.com>
2015-10-22 10:45:06 -0700

    Initial Commit - autoloader + base class

---
+++ src/Taxify.php
@@ -0,0 +1,86 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tony DeStefano
+ * Date: 10/22/15
+ * Time: 10:31 AM
+ */
+
+namespace ZayconTaxify;
+
+class Taxify {
+
+	const DEV_URL = 'https://ws-test.shipcompliant.com/taxify/1.1/core/JSONservice.asmx/';
+	const PROD_URL = 'https://shipcompliant.com/taxify/1.1/core/JSONservice.asmx/';
+
+	private $url;
+	private $api_key;
+
+	/**
+	 * @param null $api_key
+	 * @param bool|FALSE $use_prod
+	 */
+	function __construct( $api_key=NULL, $use_prod=FALSE ) {
+
+		if ( $api_key !== NULL )
+		{
+			$this->setApiKey( $api_key );
+		}
+
+		$this->url = ( $use_prod === TRUE ) ? self::PROD_URL : self::DEV_URL;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isProd()
+	{
+		return $this->url == self::PROD_URL;
+	}
+
+	/**
+	 * @return bool
+	 */
+	public function isDev()
+	{
+		return $this->url == self::DEV_URL;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getEnvironment()
+	{
+		return ($this->url == self::PROD_URL) ? 'prod' : 'dev';
+	}
+
+	/**
+	 * @return mixed
+	 */
+	public function getApiKey()
+	{
+		return $this->api_key;
+	}
+
+	/**
+	 * @param mixed $api_key
+	 *
+	 * @return Taxify
+	 */
+	public function setApiKey( $api_key )
+	{
+		$this->api_key = $api_key;
+
+		return $this;
+	}
+
+	/**
+	 * @return string
+	 */
+	public function getUrl()
+	{
+		return $this->url;
+	}
+
+	
+}
---
+++ src/taxify_required.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Tony DeStefano
+ * Date: 10/22/15
+ * Time: 10:22 AM
+ */
+
+spl_autoload_register(function($class)
+{
+	$class = str_replace('ZayconTaxify\\', '', $class);
+
+	if (file_exists(__DIR__.'/'.$class.'.php'))
+	{
+		require_once(__DIR__.'/'.$class.'.php');
+	}
+});
