db13c3d07075d82dbaa0c0c7bad031eb0fc91699
Tony DeStefano <tony@zayconfoods.com>
2015-10-22 11:20:09 -0700

    Generic Exception class that extends \Exception, just in case we want to add stuff later

--- src/Exception.php
+++ src/Exception.php
@@ -5,3 +5,15 @@
  * Date: 10/22/15
  * Time: 11:10 AM
  */
+
+namespace ZayconTaxify;
+
+class Exception extends \Exception {
+
+	const ERROR_COMMUNICATION = 'Communication error with the server';
+
+	function __construct( $message, $code=0, \Exception $previous=NULL) {
+
+		parent::__construct( $message, $code, $previous );
+	}
+}
