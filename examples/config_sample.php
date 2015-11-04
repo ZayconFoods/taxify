adb72c2ae215eece656e001d407d7a54ed951abf
Tony DeStefano <spokanetony@gmail.com>
2015-11-04 10:46:05 -0800

    Added config_sample.php, removed config.php, updated .gitignore to exclude config.php

--- examples/config_sample.php
+++ examples/config_sample.php
@@ -4,6 +4,8 @@
  * User: Tony DeStefano
  * Date: 10/22/15
  * Time: 2:23 PM
+ *
+ * Add your API Key and rename this file to config.php
  */
 
 error_reporting( E_ALL );
@@ -11,4 +13,4 @@ ini_set( 'display_errors', 1 );
 
 require_once( dirname( __DIR__ ) . '/src/taxify_required.php' );
 
-define( 'API_KEY', '[YOUR API KEY]');
+define( 'API_KEY', '[YOUR_API_KEY]');
